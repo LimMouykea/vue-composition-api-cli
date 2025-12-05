ace31e887e028aed0f61507d35acc5af7fb82ca8
LimMouykea <limmouykea65@gmail.com>
2025-12-05 14:20:16 +0700

    Composition API with ref , reactive concept

---
+++ first-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Number is: {{ number }}</h1>
+    <h1>object :  {{ object.name }}</h1>
+    <p>{{ text }}</p>
+    <button @click="increaseNum">Increase Number</button>
+    <button @click="changeText">Change Text</button>
+    <button @click="updateObject">Change Object</button>
+  </div>
+</template>
+<script setup>
+import { ref , reactive } from 'vue';
+let number = ref(0);
+let text = ref("Hello Vue 3!");
+let object = reactive({ name: "Vue", version: 3 });
+function increaseNum() {
+  number.value++;
+}
+function changeText() {
+  text.value = "Text Changed!";
+}
+function updateObject() {
+  object.name = "Vue.js";
+}
+</script>
